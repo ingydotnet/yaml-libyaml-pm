1f1204888b3963f78a3fd1c04f162368423a6db3
Ingy döt Net <ingy@ingy.net>
2014-08-15 21:07:26 -0700

    CPAN Release 0.48
    - Add t/000-compile-modules.t

--- lib/YAML/LibYAML.pm
+++ lib/YAML/LibYAML.pm
@@ -1,7 +1,8 @@
 use strict; use warnings;
 package YAML::LibYAML;
-our $VERSION = '0.47';
 
-die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
+sub import {
+    die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
+}
 
 1;
--- lib/YAML/XS.pm
+++ lib/YAML/XS.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::XS;
-our $VERSION = '0.47';
+our $VERSION = '0.48';
 
 use base 'Exporter';
 
