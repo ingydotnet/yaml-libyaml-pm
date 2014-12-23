0ff1b62d7750f9ea5347fb7cc937bafcf1de70b3
Ingy döt Net <ingy@ingy.net>
2014-12-22 17:09:25 -0800

    Refactor the updater bash script

--- LibYAML/update.sh
+++ LibYAML/update.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 libyaml=../../libyaml
-source=$libyaml/src
-include=$libyaml/include
+source="$libyaml/src"
+include="$libyaml/include"
 need_branch=perl-yaml-xs
 
-if [ ! -d $source ]; then
-    echo "'$source' does not exist"
-    exit 1
+if [ ! -d "$source" ]; then
+  echo "'$source' does not exist"
+  exit 1
 fi
 
-get_branch() {
-    (
-        cd $libyaml
-        echo $(git rev-parse --abbrev-ref HEAD)
-    )
-}
-
-libyaml_branch=$(get_branch)
+libyaml_branch="$(
+  cd "$libyaml"
+  git rev-parse --abbrev-ref HEAD
+)"
 
 if [ "$libyaml_branch" != "$need_branch" ]; then
-    echo "libyaml must be set to branch '$need_branch'"
-    exit 1
+  echo "libyaml must be set to branch '$need_branch'"
+  exit 1
 fi
 
-diff=$(diff -q $source .| grep -v '^Only';diff -q $include .| grep -v '^Only')
+diff="$(
+  diff -q $source . |
+  grep -v '^Only'
+  diff -q $include . |
+  grep -v '^Only'
+)"
 
 if [ -n "$diff" ]; then
-    echo "*** Updating from libyaml repository ***"
-    diff -q $source .| grep -v '^Only'
-    cp $source/*.{c,h} .
-    diff -q $include .| grep -v '^Only'
-    cp $include/*.h .
+  echo "*** Updating from libyaml repository ***"
+  diff -q "$source" . | grep -v '^Only'
+  cp "$source"/*.{c,h} .
+  diff -q "$include" . | grep -v '^Only'
+  cp "$include"/*.h .
 fi
