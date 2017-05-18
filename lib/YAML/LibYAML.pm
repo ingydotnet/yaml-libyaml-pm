da7964fb970577e716da82626299c7a4c9274735
Tina Müller <cpan2@tinita.de>
2017-05-18 23:10:07 +0200

    CPAN Release 0.65
    - Prevent warning about unused variables (see issue#59)
    - Clarify documentation about exported functions

--- lib/YAML/LibYAML.pm
+++ lib/YAML/LibYAML.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package YAML::LibYAML;
-our $VERSION = '0.64';
+our $VERSION = '0.65';
 
 sub import {
     die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
--- lib/YAML/XS.pm
+++ lib/YAML/XS.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::XS;
-our $VERSION = '0.64';
+our $VERSION = '0.65';
 
 use base 'Exporter';
 
