d17049c2836e63c48c119e7e54d887ce457058a1
Tina Müller <cpan2@tinita.de>
2017-11-14 21:02:56 +0100

    CPAN Release 0.66_002
    - Apply PR#67 (TINITA++)
    - Support standard tags !!str, !!map and !!seq instead of dying

--- lib/YAML/LibYAML.pm
+++ lib/YAML/LibYAML.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package YAML::LibYAML;
-our $VERSION = '0.66_001';
+our $VERSION = '0.66_002';
 
 sub import {
     die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
--- lib/YAML/XS.pm
+++ lib/YAML/XS.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::XS;
-our $VERSION = '0.66_001';
+our $VERSION = '0.66_002';
 
 use base 'Exporter';
 
