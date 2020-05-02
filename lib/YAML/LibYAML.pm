8838f8b3a53a5d64071a76831c22e6ff99b4b501
Tina Müller <cpan2@tinita.de>
2020-05-02 20:39:19 +0200

    CPAN Release 0.82
    - Updated libyaml sources to 0.2.4. Changes affecting YAML::XS are
    - Output '...' at the stream end after a block scalar with trailing empty lines
    - Accept '%YAML 1.2' directives (they are ignored and do not change behaviour though)

--- lib/YAML/LibYAML.pm
+++ lib/YAML/LibYAML.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package YAML::LibYAML;
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 
 sub import {
     die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
--- lib/YAML/XS.pm
+++ lib/YAML/XS.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::XS;
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 
 use base 'Exporter';
 
