918652f663946a21bf37d6d5a4b42a36d00d7816
Ingy döt Net <ingy@ingy.net>
2023-05-04 21:37:54 +0200

    CPAN Release 0.87
    - Turn off internal POK flag for number scalars

--- lib/YAML/LibYAML.pm
+++ lib/YAML/LibYAML.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package YAML::LibYAML;
-our $VERSION = '0.86_001';
+our $VERSION = '0.87';
 
 sub import {
     die "YAML::LibYAML has been renamed to YAML::XS. Please use YAML::XS instead.";
--- lib/YAML/XS.pm
+++ lib/YAML/XS.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::XS;
-our $VERSION = '0.86_001';
+our $VERSION = '0.87';
 
 use base 'Exporter';
 
